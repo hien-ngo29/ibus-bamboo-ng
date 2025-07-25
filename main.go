84ace6fabf95c8eb608ddbf9bf2d96725c0119cb
Ngo Phu Hien <ngophuhien029@gmail.com>
2025-07-25 15:20:01 +0700

    Change the name and version

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ import (
 
 const (
 	ComponentName = "org.freedesktop.IBus.bamboo"
-	EngineName    = "Bamboo"
+	EngineName    = "Bamboo NG"
 )
 
 var embedded = flag.Bool("ibus", false, "Run the embedded ibus component")
